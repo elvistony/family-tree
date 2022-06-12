4ec3aa602f34b264356d796d36259a1e27a697de
Elvis Tony <elvistonyrency@gmail.com>
2022-06-12 08:04:23 -0700

    Changed URL Source

--- assets/js/dataloader.js
+++ assets/js/dataloader.js
@@ -4,6 +4,6 @@ json_load.onload = function() {
     callback_after_load()
 }
 URL = '../assets/data/family.json';
-URL = "https://script.googleusercontent.com/macros/echo?user_content_key=Cbk-7jgfNj0ifpUWINqOJhMycHFKcQ_QYZuqbu_INTeZz0D_rn0A2cRgbbXvO47VVdhpv2xtW7uLBjxLzNeWe0AyFEfy00GUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMPYdn_qKLx7MPeDOVe124Donyo1yrE701EWsraKblVOEL62pnWyY5K0gQka4RKmNUJfd2C6q4t3jbgEoScS7JoAmBwbTW0bRtz9Jw9Md8uu&lib=MbRtGIS41UfKvxlJpHpTqAVHEBcGLnv5U";
+URL = "https://script.google.com/macros/s/AKfycbz1lq5OjuEctWs5KUppm_YkBWM-Sr73JJ_AV2qr8KGHCGE0peeLS_0h6yU6o0vWH6lZcw/exec";
 json_load.open("GET", URL);
 json_load.send();
