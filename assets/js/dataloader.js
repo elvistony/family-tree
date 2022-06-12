19e9bdc1dc9e15dcd422941ab49e6ab2bfe91e98
Elvis Tony <elvistonyrency@gmail.com>
2022-06-12 08:06:44 -0700

    Reverted back to JSON

--- assets/js/dataloader.js
+++ assets/js/dataloader.js
@@ -4,6 +4,7 @@ json_load.onload = function() {
     callback_after_load()
 }
 URL = '../assets/data/family.json';
-URL = "https://script.google.com/macros/s/AKfycbz1lq5OjuEctWs5KUppm_YkBWM-Sr73JJ_AV2qr8KGHCGE0peeLS_0h6yU6o0vWH6lZcw/exec";
+// CORS error when accessing GAS API
+// URL = "https://script.google.com/macros/s/AKfycbz1lq5OjuEctWs5KUppm_YkBWM-Sr73JJ_AV2qr8KGHCGE0peeLS_0h6yU6o0vWH6lZcw/exec";
 json_load.open("GET", URL);
 json_load.send();
